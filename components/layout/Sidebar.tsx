a2fbb804d664813c4f50f0e19c44265ade1e23ea
mahmoud493 <mbouamrirene493@gmail.com>
2026-06-17 22:29:26 +0100

    fix: build successful

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { classNames } from 'classnames';
+import classNames from 'classnames';
 
 const navigation = [
   { name: 'Dashboard', href: '/dashboard', icon: '📊' },
